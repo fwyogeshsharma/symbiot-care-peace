6956fb56ae7dd0265504bb26727d38ae1b700bf1
Aman <yadavaman2282000@gmail.com>
2025-12-29 14:58:02 +0530

    report subscription date input field made right

---
+++ src/components/ui/time-picker.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+
+interface TimePickerProps extends Omit<React.ComponentProps<"input">, "type"> {
+  value?: string;
+  onChange?: (value: string) => void;
+}
+
+const TimePicker = React.forwardRef<HTMLInputElement, TimePickerProps>(
+  ({ className, value, onChange, ...props }, ref) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (onChange) {
+        onChange(e.target.value);
+      }
+    };
+
+    return (
+      <Input
+        type="time"
+        className={cn("w-full", className)}
+        value={value}
+        onChange={handleChange}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+TimePicker.displayName = "TimePicker";
+
+export { TimePicker };
