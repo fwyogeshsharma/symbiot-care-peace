84f3d115a31955aa1e0896db15ba565ae00fb666
Aman <yadavaman2282000@gmail.com>
2026-02-06 15:11:30 +0530

    dialog corrected

--- src/components/help/VideoDialog.tsx
+++ src/components/help/VideoDialog.tsx
@@ -1,6 +1,4 @@
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
-import { X } from 'lucide-react';
-import { Button } from '@/components/ui/button';
 
 interface VideoDialogProps {
   open: boolean;
@@ -39,17 +37,7 @@ export const VideoDialog = ({ open, onOpenChange, youtubeUrl, title }: VideoDial
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="max-w-4xl w-[90vw] p-0">
         <DialogHeader className="p-6 pb-4">
-          <div className="flex items-center justify-between">
-            <DialogTitle className="pr-8">{title}</DialogTitle>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="h-8 w-8 shrink-0"
-              onClick={() => onOpenChange(false)}
-            >
-              <X className="h-4 w-4" />
-            </Button>
-          </div>
+          <DialogTitle>{title}</DialogTitle>
         </DialogHeader>
         <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
           <iframe
